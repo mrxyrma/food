8dd389b99079bf9de41a2487a6887c369138d473
Andrey Korolev <ander_kor@mail.ru>
2022-11-07 00:18:59 +0400

    ДЗ из 4 части 3 урока

--- js/script.js
+++ js/script.js
@@ -1,102 +1,37 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
-'use strict';
-
 document.addEventListener('DOMContentLoaded', () => {
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
-    
-    const adv = document.querySelectorAll('.promo__adv img'),
-          poster = document.querySelector('.promo__bg'),
-          genre = poster.querySelector('.promo__genre'),
-          movieList = document.querySelector('.promo__interactive-list'),
-          addForm = document.querySelector('form.add'),
-          addInput = addForm.querySelector('.adding__input'),
-          checkbox = addForm.querySelector('[type="checkbox"]');
-    
-    const deleteAdv = (arr) => {
-        arr.forEach(item => {
-            item.remove();
-        });
-    };
+  const tabsParent = document.querySelector('.tabheader__items'),
+        tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent');
 
-    deleteAdv(adv);
 
-    const makeChanges = () => {
-        genre.textContent = 'драма';
-        poster.style.backgroundImage = 'url("./img/bg.jpg")';
-    };
-
-    makeChanges();
-
-    const sortArr = (arr) => {
-        arr.sort();
-    };
-    
-    sortArr(movieDB.movies);
-
-    function createMovieList(films, parent) {
-        parent.innerHTML = '';
-        sortArr(films);
-
-        films.forEach((film, i) => {
-            parent.innerHTML += `<li class="promo__interactive-item"> ${i+1} ${film} 
-                                        <div class="delete"></div>
-                                    </li>`;
-        });
-
-        document.querySelectorAll('.delete').forEach((item, i) => {
-            item.addEventListener('click', () => {
-                item.parentElement.remove();
-                movieDB.movies.splice(i, 1);
-                createMovieList(movieDB.movies, movieList);
-            });
-        });
-    }
-
-    createMovieList(movieDB.movies, movieList);
-
-    addForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        let newFilm = addInput.value.trim(),
-            favourite = checkbox.checked;
-        
-        if (newFilm) {
-            if (newFilm.length > 21) {
-                newFilm = `${newFilm.substring(0, 22)}...`;
-            }
-
-            if (favourite) {
-                console.log("Добавляем любимый фильм");
-            }
-
-            movieDB.movies.push(newFilm);
-            createMovieList(movieDB.movies, movieList);
-            event.target.reset();
-        }
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
     });
 
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (item == target) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+    });
 });
