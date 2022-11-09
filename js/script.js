0ad7778a56b2a9d48453251a2c9b3c0e0dd84eca
Andrey Korolev <ander_kor@mail.ru>
2022-11-09 21:02:46 +0400

    ДЗ из 4 части 18 урока

--- js/script.js
+++ js/script.js
@@ -110,7 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function closeModal() {
       modal.style.display = '';
-      document.body.style.overflow = '';
+      document.body.style.overflow = ''; 
     }
 
     modal.addEventListener('click', (event) => {
@@ -133,4 +133,56 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
     window.addEventListener('scroll', showModalScroll);
+
+    //Class
+    class MenuCard {
+      constructor(src, alt, title, descr, price, parent) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parent);
+      }
+
+      addInfo() {
+        const element = document.createElement('div');
+        element.innerHTML = `
+          <div class="menu__item">
+              <img src=${this.src} alt=${this.alt}>
+              <h3 class="menu__item-subtitle">${this.title}</h3>
+              <div class="menu__item-descr">${this.descr}</div>
+              <div class="menu__item-divider"></div>
+              <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+              </div>
+          </div>
+        `;
+        this.parent.append(element); 
+      }
+    }
+    new MenuCard('img/tabs/vegy.jpg',
+    'vegy',
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    229,
+    '.menu .container').addInfo();
+
+    new MenuCard('img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    550,
+    '.menu .container').addInfo();
+
+    new MenuCard(
+      'img/tabs/post.jpg',
+      'post',
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+      430,
+      '.menu .container'
+    ).addInfo();
+
 });
